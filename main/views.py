aa91b23eaf078b225e1b35b27d9ee54afb75f8a2
Aleksey <Aleksey4925@proton.me>
2024-08-28 17:11:35 +0300

    Created main index and about pages

--- main/views.py
+++ main/views.py
@@ -6,3 +6,12 @@ def index(request):
     context = {"title": "Главная", "content": 'Главная страница приложения "Notify Me"'}
 
     return render(request, "main/index.html", context)
+
+
+def about(request):
+    context = {
+        "title": "О приложении",
+        "content": 'Здесь можно узнать цели создания приложения "Notify Me"',
+    }
+
+    return render(request, "main/about.html", context)
