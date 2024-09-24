5ea168fe7ca481ddbd1e992d25d0605bc06cf9f7
Aleksey <aleksey4925@proton.me>
2024-09-24 09:54:24 +0000

    staticfiles folder was removed

--- notify_me/settings.py
+++ notify_me/settings.py
@@ -16,6 +16,10 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+## Load 'environment-variables-for-web-apps':
+# from dotenv import load_dotenv
+# project_folder = os.path.expanduser('~/NotifyMe')
+# load_dotenv(os.path.join(project_folder, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
